fbe6c6f7600b3fef1c6ab5f60b4dcadd7957e0d4
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-06 11:02:26 -0300

    Add files via upload

---
+++ idade minima para cnh/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace idade_minima_para_cnh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.Write("Por favor, informe a sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+            {
+                Console.Write("parabéns, você ja pode tirar a sua CNH.");
+            }
+            else
+            {
+                Console.Write("Desculpe, a idade mínima para tirar a CNH é de 18 anos.");
+            }
+        }
+    }
+}
